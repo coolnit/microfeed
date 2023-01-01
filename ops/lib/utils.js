14c07cb2b72c9a8dc50e36bc59fdc397a6be719c
Nitin Nagwekar <nnitin@hotmail.com>
2023-01-01 15:37:48 -0500

    Update utils.js
    Fixing error: ✘ [ERROR] A request to the Cloudflare API (/accounts/***/d1/database) failed.

--- ops/lib/utils.js
+++ ops/lib/utils.js
@@ -53,14 +53,16 @@ class WranglerCmd {
 
   createFeedDb() {
     const wranglerCmd = this.currentEnv !== 'development' ?
-      `wrangler d1 create ${this._non_dev_db()}` : 'echo "FEED_DB"';
+//       `wrangler d1 create ${this._non_dev_db()}` : 'echo "FEED_DB"';
+      `wrangler r2 create ${this._non_dev_db()}` : 'echo "FEED_DB"';
     console.log(wranglerCmd);
     return this._getCmd(wranglerCmd);
   }
 
   createFeedDbTables() {
     const dbName = this.currentEnv !== 'development' ? this._non_dev_db() : 'FEED_DB --local';
-    const wranglerCmd = `wrangler d1 execute ${dbName} --file ops/db/init.sql`;
+//     const wranglerCmd = `wrangler d1 execute ${dbName} --file ops/db/init.sql`;
+    const wranglerCmd = `wrangler r2 execute ${dbName} --file ops/db/init.sql`;
     console.log(wranglerCmd);
     return this._getCmd(wranglerCmd);
   }
